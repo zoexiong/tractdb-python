2fb32eb06dd12846c25391ca2857bfc6506a0da3
James Fogarty <jayfo.github@gmail.com>
2016-08-19 20:51:07 -0700

    Move CouchDB Logic to Accounts

--- tests/test_server_accounts.py
+++ tests/test_server_accounts.py
@@ -1,24 +1,10 @@
-import os
-import subprocess
-import tractdb.admin
+import tests.docker_base as docker_base
+import tractdb.server.accounts
 import unittest
 
 
-BASE_DOCKER_IP = None
-
-
 def setup():
-    global BASE_DOCKER_IP
-
-    if 'BASE_DOCKER_ON_TRAVIS' in os.environ:
-        BASE_DOCKER_IP = 'localhost'
-    else:
-        result = subprocess.run(
-            ['invoke', 'docker_machine_ip'],
-            stdout=subprocess.PIPE
-        )
-
-        BASE_DOCKER_IP = result.stdout.decode('utf-8', 'backslashreplace').strip()
+    pass
 
 
 def teardown():
@@ -28,10 +14,12 @@ def teardown():
 class TestTractDBAdmin(unittest.TestCase):
     @property
     def admin(self):
-        return tractdb.admin.TractDBAdmin(
-            server_url='http://{}:{}'.format(BASE_DOCKER_IP, 5984),
-            server_admin='docker-couchdb-test-admin',
-            server_password='docker-couchdb-test-admin-password'
+        return tractdb.server.accounts.AccountsAdmin(
+            couchdb_url='http://{}:5984'.format(
+                docker_base.ip()
+            ),
+            couchdb_admin='docker-couchdb-test-admin',
+            couchdb_admin_password='docker-couchdb-test-admin-password'
         )
 
     def test_create_delete_account(self):
