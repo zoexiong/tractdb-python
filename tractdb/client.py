702dc750e843ac2a4f69a10e7ae89bbf82a0a172
zoexiong <xiongsy@uw.edu>
2016-11-13 22:17:14 -0800

    add roles part to client.py

--- tractdb/client.py
+++ tractdb/client.py
@@ -12,7 +12,22 @@ class TractDBClient(object):
         self._client_account = client_account
         self._client_account_password = client_account_password
 
-    # def add_role(self, account, role):
+    def add_role(self, account, role):
+        """ Create a role.
+        """
+
+        response = requests.post(
+            '{}/{}/{}/{}'.format(
+                self._tractdb_url,
+                'account',
+                account,
+                'roles'
+            ),
+            json={
+                'account': account,
+                'role': role
+            }
+        )
 
     def create_account(self, account, account_password):
         """ Create an account.
@@ -43,7 +58,19 @@ class TractDBClient(object):
 
         print(response)
 
-    # def delete_role(self, account, role):
+    def delete_role(self, account, role):
+        """ Delete a role.
+        """
+
+        response = requests.delete(
+            '{}/{}/{}/{}/{}'.format(
+                self._tractdb_url,
+                'account',
+                account,
+                'roles',
+                role
+            )
+        )
 
     def list_accounts(self):
         """ List our accounts.
@@ -60,4 +87,19 @@ class TractDBClient(object):
 
         return json['accounts']
 
-    # def list_roles(self, account):
+    def list_roles(self, account):
+        """ List of roles of the given account.
+        """
+
+        response = requests.get(
+            '{}/{}/{}/{}'.format(
+                self._tractdb_url,
+                'accounts',
+                account,
+                'roles'
+            )
+        )
+
+        json = response.json()
+
+        return json['roles']
